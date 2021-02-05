3d615bb8abb6c3d990102219e3a8ed9b4730ab4c
jswini <jswinikoff@gmail.com>
2021-02-05 04:01:30 +0000

    first commit

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - prints 2 digit ints with commas
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int num;
+	int tens;
+
+	for (tens = 0; tens < 10; tens++)
+		for (num = 0; num < 10; num++)
+		{
+		putchar(tens + '0');
+		putchar(num + '0');
+		if (tens < 10 && num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
