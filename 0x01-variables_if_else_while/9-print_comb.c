d5a912587e9299172cac21a7bc8375e416db3388
jswini <jswinikoff@gmail.com>
2021-02-05 03:50:48 +0000

    first commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - prints single digit ints with commas
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
