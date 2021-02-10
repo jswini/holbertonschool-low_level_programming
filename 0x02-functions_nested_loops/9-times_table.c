7e286291c5c8d4491954ce4e770a8fc6fbe8984c
jswini <jswinikoff@gmail.com>
2021-02-10 16:32:52 +0000

    fixed spacing for betty

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,23 +9,24 @@ void times_table(void)
 	int a;
 	int b;
 	int result;
-	for(a = 0; a < 10; a++)
+
+	for (a = 0; a < 10; a++)
 	{
-		for(b = 0; b < 10; b++)
+		for (b = 0; b < 10; b++)
 		{
 			result = a * b;
-			if((result/10) == 0)
+			if ((result / 10) == 0)
 			{
-				if(b > 0)
+				if (b > 0)
 					_putchar(' ');
-				_putchar(result%10 + '0');
+				_putchar(result % 10 + '0');
 			}
 			else
 			{
-				_putchar((result/10) + '0');
-				_putchar((result%10) + '0');
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
 			}
-			if(b < 9)
+			if (b < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
