04229af084d0e90b4f8aa39c34d51c4dd4dab89f
jswini <jswinikoff@gmail.com>
2021-02-10 15:45:25 +0000

    changed to 2 loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,28 +8,18 @@
 void jack_bauer(void)
 {
 	int min1;
-	int min2;
 	int hour1;
-	int hour2 = 0;
 
-	for (hour2 = 0; hour2 < 3; hour2++)
-	{
+		for (hour1 = 0; hour1 < 23; hour1++)
 		{
-			for (hour1 = 0; hour1 < 4; hour1++)
+			for (min1 = 0; min1 < 60; min1++)
 			{
-				for (min2 = 0; min2 < 6; min2++)
-				{
-					for (min1 = 0; min1 < 10; min1++)
-					{
-						_putchar(hour2 + '0');
-						_putchar(hour1 + '0');
-						_putchar(':');
-						_putchar(min2 + '0');
-						_putchar(min1 + '0');
-						_putchar('\n');
-					}
-				}
+				_putchar((hour1/10) + '0');
+				_putchar((hour1%10) + '0');
+				_putchar(':');
+				_putchar((min1/10) + '0');
+				_putchar((min1%10) + '0');
+				_putchar('\n');
 			}
 		}
-	}
 }
