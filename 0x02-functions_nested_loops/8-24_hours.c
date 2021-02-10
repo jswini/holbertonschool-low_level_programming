bf82d625796efa7fd42050b99c15aa0fbb8a0154
jswini <jswinikoff@gmail.com>
2021-02-10 15:48:29 +0000

    corrected betty and updated loop exit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,15 +10,15 @@ void jack_bauer(void)
 	int min1;
 	int hour1;
 
-		for (hour1 = 0; hour1 < 23; hour1++)
+		for (hour1 = 0; hour1 < 24; hour1++)
 		{
 			for (min1 = 0; min1 < 60; min1++)
 			{
-				_putchar((hour1/10) + '0');
-				_putchar((hour1%10) + '0');
+				_putchar((hour1 / 10) + '0');
+				_putchar((hour1 % 10) + '0');
 				_putchar(':');
-				_putchar((min1/10) + '0');
-				_putchar((min1%10) + '0');
+				_putchar((min1 / 10) + '0');
+				_putchar((min1 % 10) + '0');
 				_putchar('\n');
 			}
 		}
