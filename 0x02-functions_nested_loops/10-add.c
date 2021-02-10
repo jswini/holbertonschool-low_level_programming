30b7209b010e99e65320d4d1206213e3d6e829fe
jswini <jswinikoff@gmail.com>
2021-02-10 16:48:34 +0000

    first commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ *add - addition of integers
+ *@a: integer 1
+ *@b: integer 2
+ *
+ *Return: result
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
