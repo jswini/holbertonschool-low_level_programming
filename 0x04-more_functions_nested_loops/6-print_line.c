7e496a0e0cb873ce393588cee750cdc8d381fa5e
jswini <jswinikoff@gmail.com>
2021-02-11 21:16:16 +0000

    corrected to have newline if input is 0

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,7 +9,12 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
-		_putchar('_');
-	_putchar('\n');
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
+	else
+		_putchar ('\n');
 }
