8557de754f7a23aea622e3ef4a5f706ac24daf99
jswini <jswinikoff@gmail.com>
2021-02-11 23:49:34 +0000

    changed loop to iterate from 1

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,14 +9,7 @@ void print_line(int n)
 {
 	int i;
 
-	if (n > 0)
-	{
-		for (i = 0; i <= n; i++)
+		for (i = 1; i <= n; i++)
 			_putchar('_');
-		_putchar('\n');
-	}
-	else
-	{
 		_putchar ('\n');
-	}
 }
