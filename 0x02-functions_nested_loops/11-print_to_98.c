d751e910627b1cb647652a82746fd67438d9e80d
jswini <jswinikoff@gmail.com>
2021-02-14 21:12:28 +0000

    corrected formatting

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,12 +12,21 @@ void print_to_98(int n)
 	if (n < 98)
 
 	{
-		for (i = n; i < 98; i++)
-			printf("%d, ", i);
-		printf("98\n");
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
 	}
 	else
-		for (i = n; i > 98; i--)
+	{
+		for (i = n; i >= 98; i--)
+		{
 			printf("%d, ", i);
-	printf("98\n");
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	printf("\n");
 }
