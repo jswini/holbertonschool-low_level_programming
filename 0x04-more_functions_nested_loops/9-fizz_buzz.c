856c238f5e4b6b0c877b168c1da382576de6fe5b
jswini <jswinikoff@gmail.com>
2021-02-15 19:59:50 +0000

    corrected spacing

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,9 +13,11 @@ int main(void)
 	{
 		if (number % 3 == 0 && number % 5 == 0)
 		printf("FizzBuzz ");
+		else if (number == 100)
+			printf("Buzz");
 		else if (number % 3 == 0)
 			printf("Fizz ");
-		else if (number % 5 == 0)
+		else if (number % 5 == 0 && number != 100)
 			printf("Buzz ");
 		else
 			printf("%d ", number);
