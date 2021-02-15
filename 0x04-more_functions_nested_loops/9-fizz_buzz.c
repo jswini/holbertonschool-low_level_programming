e4c9f4eb4c3e9212487395167a753dec09f3292a
jswini <jswinikoff@gmail.com>
2021-02-15 19:53:26 +0000

    first commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *main - outputs ins to 100 with fizz or buzz subbed for some digits
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 1; number <= 100; number++)
+	{
+		if (number % 3 == 0 && number % 5 == 0)
+		printf("FizzBuzz ");
+		else if (number % 3 == 0)
+			printf("Fizz ");
+		else if (number % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", number);
+	}
+	printf("\n");
+	return (0);
+}
