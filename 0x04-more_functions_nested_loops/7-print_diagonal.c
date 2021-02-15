371c2956e00ab3a05ae9c96a6716e585f4f1a61e
jswini <jswinikoff@gmail.com>
2021-02-15 19:09:50 +0000

    First commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ *print_diagonal - outputs spaces and a slash to create a diagonal line
+ *@n: number of spaces to print
+ *
+ *Return: void
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
