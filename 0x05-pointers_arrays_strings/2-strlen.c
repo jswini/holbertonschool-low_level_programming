0caeac9f53a7d215e28bb531f0784893de5d3de6
jswini <jswinikoff@gmail.com>
2021-02-16 21:34:33 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *_strlen - outputs the length of the string s
+ *@s: string passed to be measured
+ *
+ *Return: length of string
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+
+	}
+		return (count);
+}
