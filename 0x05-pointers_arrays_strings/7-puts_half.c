932b634df14a3917aef4d8d7f4811a92bc959a08
jswini <jswinikoff@gmail.com>
2021-02-17 23:31:21 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+int _strlen(char *s);
+/**
+ *puts_half - outputs the 2nd half of the string passed to the function
+ *@str: string passed to the function
+ *
+ *Return: void
+ */
+
+void puts_half(char *str)
+{
+	int len;
+	int i;
+
+	len = _strlen(str);
+	if ((len % 2) == 0)
+	{
+		for (i = (len / 2); i <= len; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		for (i = ((len - 1) / 2); i <= len; i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
+/**
+ *_strlen - outputs the length of the string s
+ *@s: string passed to be measured
+ *
+ *Return: length of string
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+
+	}
+	return (count);
+}
