4994d896516f3f589db28db88d5ac030f1e93182
jswini <jswinikoff@gmail.com>
2021-02-18 00:00:41 +0000

    changed from subtract index to add

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,14 +13,14 @@ void puts_half(char *str)
 	int i;
 
 	len = _strlen(str);
-	if ((len % 2) == 0)
+	if ((len % 2) != 0)
 	{
-		for (i = (len / 2); i <= len; i++)
+		for (i = ((len + 1) / 2); i <= len; i++)
 			_putchar(str[i]);
 	}
 	else
 	{
-		for (i = ((len - 1) / 2); i <= len; i++)
+		for (i = (len / 2); i <= len; i++)
 			_putchar(str[i]);
 	}
 	_putchar('\n');
