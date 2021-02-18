9dbb35269cce32c4a6657598c5684fe8c7f74e73
jswini <jswinikoff@gmail.com>
2021-02-18 00:31:04 +0000

    removed output of null character

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,12 +16,18 @@ void puts_half(char *str)
 	if ((len % 2) != 0)
 	{
 		for (i = ((len + 1) / 2); i <= len; i++)
-			_putchar(str[i]);
+		{
+			if (str[i] != '\0')
+				_putchar(str[i]);
+		}
 	}
 	else
 	{
 		for (i = (len / 2); i <= len; i++)
-			_putchar(str[i]);
+		{
+			if (str[i] != '\0')
+				_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
