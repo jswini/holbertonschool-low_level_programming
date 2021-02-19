46471b5dbf3e74ca63a0fcb9600dde353e259495
jswini <jswinikoff@gmail.com>
2021-02-19 17:26:04 +0000

    adjusted to print 5 elements only

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,15 +11,14 @@
 void print_array(int *a, int n)
 {
 	int i;
+
+	if (n > 0)
 	{
-		if (n > 0)
+		for (i = 0; i < (n - 1); i++)
 		{
-			for (i = 0; i < n; i++)
-			{
-				printf("%d, ", *(a + (i - 1)));
-			}
-			printf("%d", *(a + (i - 1)));
+			printf("%d, ", *(a + i));
 		}
+		printf("%d", *(a + i));
 	}
 	printf("\n");
 }
