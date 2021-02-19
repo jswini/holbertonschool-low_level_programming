18144d102abc3084b9e3e510e4db0d939f3e15b5
jswini <jswinikoff@gmail.com>
2021-02-19 17:13:21 +0000

    fixed null character

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,10 +11,15 @@
 void print_array(int *a, int n)
 {
 	int i;
-
-	for (i = 0; i < (n - 1); i++)
 	{
-		printf("%d, ", *(a + i));
+		if (n > 0)
+		{
+			for (i = 0; i < n; i++)
+			{
+				printf("%d, ", *(a + (i - 1)));
+			}
+			printf("%d", *(a + (i - 1)));
+		}
 	}
-	printf("%d\n", *(a + (n - 1)));
+	printf("\n");
 }
