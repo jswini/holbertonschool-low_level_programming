d6ebe7c3d3ac75fbfaf3a4f47f4645693223f45a
jswini <jswinikoff@gmail.com>
2021-02-22 16:19:34 +0000

    added option for str termination

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,9 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 
-	for (i = 0; i < n && src[n] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
