e2605d25e485fa85ebf668cac3c28c7d0ab8d3d8
jswini <jswinikoff@gmail.com>
2021-02-22 16:39:16 +0000

    first commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *_strcmp - compares 2 strings and returns the ascii value difference if any
+ *@s1: first string to compare
+ *@s2: second string to compare
+ *
+ *Return: different in ascii values
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, result;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		if (s1[i] == s2[i])
+			continue;
+		else
+			break;
+	}
+	result = s1[i] - s2[i];
+	return (result);
+}
