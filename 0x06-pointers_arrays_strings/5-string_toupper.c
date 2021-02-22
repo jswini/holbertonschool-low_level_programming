b9879599ef90560ac39ce8f4c9a2a2f778f311cc
jswini <jswinikoff@gmail.com>
2021-02-22 22:10:46 +0000

    first commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *string_toupper - changes each element in a string to upper case
+ *@array: string to be changed
+ *
+ *Return: updated string
+ */
+
+char *string_toupper(char *array)
+{
+	int i;
+
+	for (i = 0; array [i] != '\0'; i++)
+	{
+		if (array[i] > 96 && array [i] < 123)
+		{
+			array[i] = array[i] - 32;
+		}
+	}
+	return (array);
+}
