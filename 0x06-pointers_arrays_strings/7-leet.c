e95e1c9c3517736219af6253a022034eed85825d
jswini <jswinikoff@gmail.com>
2021-02-23 03:03:22 +0000

    first commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *leet - subs leet char for letters
+ *@array: array to be changed
+ *
+ *Return: character array
+ */
+
+char *leet(char *array)
+{
+	char alpha[] = "aeotlAEOTL";
+	char nums[] = "4307143071";
+	int n, x;
+
+	for (n = 0; array[n] != '\n'; n++)
+	{
+		for (x = 0; alpha[x] != '\0'; x++)
+		{
+			if (array[n] == alpha[x])
+				array[n] = nums[x];
+		}
+	}
+	return (array);
+}
