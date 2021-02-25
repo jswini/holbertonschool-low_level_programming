6c06aed05e2b55e5b0f883729e7af316f3ed26cd
jswini <jswinikoff@gmail.com>
2021-02-25 20:33:39 +0000

    first commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ *betty comments to add
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
