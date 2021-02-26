5cce456ac1edb9cdd5ff1bc16e9e2f54cf07733a
jswini <jswinikoff@gmail.com>
2021-02-26 20:41:34 +0000

    first commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *_memset - changes value at a memory address
+ *@s: array passed in
+ *@b: new value
+ *@n: number of elements to change
+ *
+ *Return: pointer to s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		*(s+i) = b;
+	}
+	return (s);
+}
