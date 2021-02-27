aa041e31479bf398aa411d1a487d27d38d11200a
jswini <jswinikoff@gmail.com>
2021-02-27 21:56:18 +0000

    first commit of strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+/**
+ *_strspn - finds the number of matching bytes in a string segment
+ *@s: string to search
+ *@accept: string or char to look for
+ *
+ *Return: number of bytes found
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, counter;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				counter++;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+			break;
+	}
+	return (counter);
+}
