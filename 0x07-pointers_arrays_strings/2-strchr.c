7bd84d4619bbcdc3f7f5b13cde6319fbc4e13c03
jswini <jswinikoff@gmail.com>
2021-02-27 23:30:06 +0000

    removed the if

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,15 +11,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	if (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (i = 0; s[i] != '\0'; i++)
+		if (s[i] == c)
 		{
-			if (s[i] == c)
-			{
-				return (s + i);
-			}
+			return (s + i);
 		}
-		return ('\0');
 	}
+	return ('\0');
 }
