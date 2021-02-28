d27ea060a89771ebad7d97801c2ef7c78df49fe4
jswini <jswinikoff@gmail.com>
2021-02-28 17:15:10 +0000

    first commit of 2 args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *main - prints all arguments passed
+ *@argc: number of arguements
+ *@argv: array of arguments
+ *
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
