247711ca7400a101e32fe3f0f565480b735263ef
jswini <jswinikoff@gmail.com>
2021-02-28 17:43:19 +0000

    first commit of mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - prints the multiplication of 2 numbers
+ *@argc: number of arguements passed
+ *@argv: array of arguments passed
+ *
+ *Return: 0 if successful, 1 if incorrect number of args
+ */
+int main(int argc, char *argv[])
+{
+	int i, prod = 1;
+
+	if (argc == 3)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			prod *= atoi(argv[i]);
+		}
+		printf("%d\n", prod);
+		return (0);
+	}
+	else
+	{
+		printf("error\n");
+		return (1);
+	}
+}
