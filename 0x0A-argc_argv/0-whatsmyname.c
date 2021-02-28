a50fb9321db5608d8f60aff285688dbf2434f8de
jswini <jswinikoff@gmail.com>
2021-02-28 17:02:58 +0000

    updated comments for betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,11 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
+ *main - prints the name of the file called
+ *@argc: number of arguments passed
+ *@argv: list of arguments passed
  *
+ *Return: 0
  */
 int main(__attribute__((unused))int argc, char *argv[])
 {
