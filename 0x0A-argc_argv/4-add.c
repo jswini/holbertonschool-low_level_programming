6b7c3e2c504a196e92f1d039238a13dce5833694
jswini <jswinikoff@gmail.com>
2021-03-02 17:47:20 +0000

    corrected for checking all elemenstrings

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+int _isalpha(int c);
 /**
  *main - adds sum of arguments if they are ints
  *@argc: number of arguments
@@ -10,7 +11,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
 	if (argc == 1)
 	{
@@ -20,9 +21,34 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
+			for (j = 0; argv[i][j]; j++)
+			{
+				if (_isalpha(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
 			sum += atoi(argv[i]);
 		}
-		printf("%d\n", sum);
 	}
+	printf("%d\n", sum);
 	return (0);
+}
+/**
+ *_isalpha - tests if input is a character
+ *
+ *@c: ascii for a character
+ *
+ *Return: 1 if character, 0 if not character
+ */
+
+int _isalpha(int c)
+{
+        if (c >= 65 && c <= 90)
+                return (1);
+        else if (c >= 97 && c <= 122)
+                return (1);
+        else
+                return (0);
 }
