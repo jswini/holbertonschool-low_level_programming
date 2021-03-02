2766751d857349c2ed230e8c00cd8037f9e46467
jswini <jswinikoff@gmail.com>
2021-03-02 17:53:54 +0000

    updated for betty

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -45,10 +45,10 @@ int main(int argc, char *argv[])
 
 int _isalpha(int c)
 {
-        if (c >= 65 && c <= 90)
-                return (1);
-        else if (c >= 97 && c <= 122)
-                return (1);
-        else
-                return (0);
+	if (c >= 65 && c <= 90)
+		return (1);
+	else if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
 }
