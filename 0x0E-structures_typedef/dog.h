d84dfc300b0546f4702853b33cf1965592511a85
jswini <jswinikoff@gmail.com>
2021-03-09 19:52:59 +0000

    first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdlib.h>
+/**
+ *struct dog - a structure to create a dog descriptor
+ *@name: char type to name the dog
+ *@age: float type to give an age to the dog
+ *@owner: char type to note the owner
+ */
+/*
+ *need to header guard
+ *need to define the structure and elements
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
