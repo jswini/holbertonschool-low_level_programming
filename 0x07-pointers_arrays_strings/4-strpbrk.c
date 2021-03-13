e0e648c05929e363e69f5f39f744c6248b1d68ee
jswini <jswinikoff@gmail.com>
2021-03-13 17:48:24 +0000

    first commit of string brk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *_strpbrk - finds the first occurance of a character in a string
+ *@s: string to search
+ *@accept: character(s) to find
+ *
+ *Return: pointer to 1st occurance or NULL if not there
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	while (*s != '\0')
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
