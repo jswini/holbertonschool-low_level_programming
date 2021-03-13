dfeb2dcde5bc7e956248942c59e46f3cceb2c222
jswini <jswinikoff@gmail.com>
2021-03-13 21:38:28 +0000

    edited spacing for betty

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -23,9 +23,9 @@ char *_strstr(char *haystack, char *needle)
 		}
 		if (needle[j] == '\0')
 		{
-			return(haystack + i);
+			return (haystack + i);
 		}
 		i++;
 	}
-	return(haystack +i);
+	return (haystack + i);
 }
