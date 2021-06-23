8db36fb9eab7ae24ecc2ebac33deda0c5fdf3d0d
jswini <jswinikoff@gmail.com>
2021-06-23 09:08:52 -0700

    first commit for using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry point to print a string
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
