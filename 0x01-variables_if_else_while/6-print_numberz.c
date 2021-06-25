dcd371a64a1873c089d34727d7c0952f6c17d9db
jswini <jswinikoff@gmail.com>
2021-06-25 11:51:14 -0700

    first commit of using putchar with numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - outputs digits using putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+		putchar(digit + '0');
+	putchar('\n');
+	return(0);
+}
