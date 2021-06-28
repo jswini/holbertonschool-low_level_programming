c67bb06bb95b96fe58b8eed60714bcd01b0f0f2b
jswini <jswinikoff@gmail.com>
2021-06-28 08:11:07 -0700

    first commit of output Holberton using _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *main - prints Holberton using _putchar
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	_putchar('H' + 0);
+	_putchar('o' + 0);
+	_putchar('l' + 0);
+	_putchar('b' + 0);
+	_putchar('e' + 0);
+	_putchar('r' + 0);
+	_putchar('t' + 0);
+	_putchar('o' + 0);
+	_putchar('n' + 0);
+	_putchar('\n');
+	return (0);
+}
