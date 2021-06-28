da4a811bb1a5316c14de1939d3f297b78c1df8a7
jswini <jswinikoff@gmail.com>
2021-06-28 08:29:26 -0700

    first commit of print alphabet 10 times with _putchar

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - outputs the alphabet 10 times using _putchar
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int counter;
+	char character;
+
+	for (counter = 0; counter <= 10; counter++)
+	{
+		for (character = 97; character <= 122; character++)
+		{
+			_putchar(character);
+		}
+		_putchar('\n');
+	}
+}
