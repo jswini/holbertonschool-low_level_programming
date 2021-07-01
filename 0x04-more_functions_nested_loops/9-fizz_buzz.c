4f88fbe820a13e0c2f766e9adb9c4819f5a47ef9
jswini <jswinikoff@gmail.com>
2021-07-01 16:31:44 -0700

    first commit of fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - outputs numbers, fizz, buzz, or fizzbuzz
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 1; number <= 100; number++)
+	{
+		if ((number % 3 == 0) && (number % 5 == 0))
+			printf("FizzBuzz ");
+		else if (number % 5 == 0)
+			printf("Buzz ");
+		else if (number % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", number);
+	}
+	return (0);
+}
