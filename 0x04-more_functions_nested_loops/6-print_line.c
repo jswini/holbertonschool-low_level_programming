65851e7f670cff3c90d5a98ff882bb8d731a9dae
jswini <jswinikoff@gmail.com>
2021-07-01 15:39:18 -0700

    correct to output proper amount of characters

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,7 +10,10 @@ void print_line(int n)
 {
 	int count;
 
-	for (count = 1; count < n; count++)
-		_putchar('_');
+	if (n != 0)
+	{
+		for (count = 0; count < n; count++)
+			_putchar('_');
+	}
 	_putchar('\n');
 }
