718e605198ec89e79ceabb5fab1fa04512c80228
jswini <jswinikoff@gmail.com>
2021-07-01 12:36:22 -0700

    first commit of is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * _isupper - checks for an uppercase letter
+ * @c: input to chec
+ *
+ * Return: 0 if uppercase, 1 if not
+ */
+int _isupper(int c)
+{
+	if ((c >= 65) && (c <= 90))
+		return (0);
+	else
+		return (1);
+}
