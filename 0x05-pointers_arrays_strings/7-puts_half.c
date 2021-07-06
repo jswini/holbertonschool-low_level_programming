70d40c8045c012754ae0829571e91059234ee7c3
jswini <jswinikoff@gmail.com>
2021-07-06 11:57:00 -0700

    first commit of puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * _strlen - determines how long a string is
+ * @s: the input string to measure
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; *(s + count) != '\0'; count++)
+	{}
+	return (count);
+}
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: input string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int length = _strlen(str) - 1;
+	int limit;
+	int count;
+
+	if (length % 2 != 0)
+		limit = (length + 1) / 2;
+	else
+		limit = length / 2;
+	for (count = limit; *(str + count); count++)
+		_putchar(*str + count);
+	_putchar('\n');
+}
