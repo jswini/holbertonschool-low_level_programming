73fc8608114b418d28d89e24541e7fdcb9580006
jswini <jswinikoff@gmail.com>
2021-07-16 09:31:42 -0700

    first commit of rev_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * reverse_array - reverses an array
+ * @a: array to reverse
+ * @n: length of array
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+	int limit = n / 2;
+
+	n -= 1;
+	for (i = 0; i <= limit; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - i];
+		a[n - i] = tmp;
+	}
+}
