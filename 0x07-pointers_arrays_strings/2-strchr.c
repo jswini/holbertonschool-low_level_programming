28cf8a47234f7bcdbf891919e2c4cfffd4169063
jswini <jswinikoff@gmail.com>
2021-07-17 10:46:02 -0700

    first commit of strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: string to search
+ * @c: character to search for
+ *
+ * Return: first occurance of c
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+		return ('\0');
+}
