7f9befda33446a9e3f1686f5e2a2054ec99c6ca4
jswini <jswinikoff@gmail.com>
2021-07-20 17:56:02 -0700

    adjusted test for null

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 	int i, j;
 	char *loc;
 
-	if (needle == NULL)
+	if (*needle == '\0')
 		return (haystack);
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
