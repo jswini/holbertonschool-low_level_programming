b9341e11e155ef448cd1714538bc1e4e37d6c3e8
jswini <jswinikoff@gmail.com>
2021-07-21 11:50:39 -0700

    first commit of alloc grid

---
+++ 0x0C-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * alloc_grid - allocates and initializes a 2d array
+ * @width: number of colums
+ * @height: number of rows
+ *
+ * Return: pointer to 2d array
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **array;
+
+	if ((width < 0) || (height < 0))
+		return (NULL);
+	array = malloc(sizeof(int) * height);
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+			return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
+		}
+	}
+	return (array);
+}
