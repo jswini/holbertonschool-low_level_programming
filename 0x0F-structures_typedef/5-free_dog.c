e74dacddd5612ad6f65cd5fe760a73e1e2f45089
jswini <jswinikoff@gmail.com>
2021-07-26 13:19:18 -0700

    first commit of free dog

---
+++ 0x0F-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees memory for struct dog_t
+ * @d: instance to free
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+/*	free(d->age);
+ */	free(d->owner);
+	free(d);
+}
