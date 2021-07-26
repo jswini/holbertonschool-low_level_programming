0b8de6d61250ab2c1dc2d271e4283306208236d8
jswini <jswinikoff@gmail.com>
2021-07-26 08:21:01 -0700

    added prototypes to header

--- 0x0F-structures-typedef/dog.h
+++ 0x0F-structures-typedef/dog.h
@@ -12,4 +12,9 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
 #endif /*DOG_H*/
