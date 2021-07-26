8cf796ad2d21412d18a2df6bacb5d68884f447d6
jswini <jswinikoff@gmail.com>
2021-07-26 12:52:55 -0700

    removed null check with a float

--- 0x0F-structures_typedef/2-print_dog.c
+++ 0x0F-structures_typedef/2-print_dog.c
@@ -15,10 +15,7 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 		d->name = nil;
 	printf("Name: %s\n", d->name);
-	if (d->age == '\0')
-		printf("Age: %s\n", nil);
-	else
-		printf("Age: %f\n", d->age);
+	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 		d->owner = nil;
 	printf("Owner: %s\n", d->owner);
