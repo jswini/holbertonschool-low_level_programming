29fff3cbbb347646ad8b3bfd23aff6fb93193b0e
jswini <jswinikoff@gmail.com>
2021-07-26 08:12:47 -0700

    first commit of dog.h

---
+++ 0x0F-structures-typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - description of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*DOG_H*/
