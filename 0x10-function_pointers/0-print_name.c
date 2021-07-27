b6ecab0b368a3c208075c150d0fb3cf21ea55f5c
jswini <jswinikoff@gmail.com>
2021-07-27 10:03:56 -0700

    first commit of print name

---
+++ 0x10-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - function that uses pointer to print a name a specific way
+ * @name: name to print
+ * @f: pointer to callback function
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
