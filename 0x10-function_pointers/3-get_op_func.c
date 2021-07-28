528b286245a4054f5e69ff7849cdfe2c4a37120d
jswini <jswinikoff@gmail.com>
2021-07-28 08:37:45 -0700

    first commit of calc

---
+++ 0x10-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * get_op_func - gets the operator and calls the proper function
+ * @s: operator passed
+ *
+ * Return: pointer to correct op_func if success, NULL if fail
+ */
+int (*get_op_func(char *s))(int a, int b)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (i < 5)
+	{
+		if (ops[i].op[0] == *s)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
