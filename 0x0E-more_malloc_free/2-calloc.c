6f8fbb42f01a8532bf22ad1ae5e7dcf6b5335e9d
jswini <jswinikoff@gmail.com>
2021-07-28 11:31:55 -0700

    adjusted loop to initialize

--- 0x0E-more_malloc_free/2-calloc.c
+++ 0x0E-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *pointer;
-	size_t bytes;
+	unsigned int bytes;
 	unsigned int i;
 
 	if ((nmemb == 0) || (size == 0))
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	for (i = 0; i < bytes; i++)
 	{
-		*pointer = '0';
+		pointer[i] = 0;
 	}
 	return (pointer);
 }
