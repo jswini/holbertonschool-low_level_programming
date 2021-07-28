29436472d724b3de9b47b9b8393ab3fe0eb763b7
jswini <jswinikoff@gmail.com>
2021-07-28 08:53:31 -0700

    moved atoi until after check for 4 args

--- 0x10-function_pointers/3-main.c
+++ 0x10-function_pointers/3-main.c
@@ -13,8 +13,6 @@ int main(int argc, char *argv[])
 	int num1, num2;
 	int (*result)(int, int);
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
 	if (argc != 4)
 	{
 		printf("Error\n");
@@ -25,6 +23,8 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 	if (((*argv[2] == '/') || (*argv[2] == '%')) && num2 == 0)
 	{
 		printf("Error\n");
