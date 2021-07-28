be4437b0159c15cc43fd58c2c03a556b0b880c98
jswini <jswinikoff@gmail.com>
2021-07-28 14:28:03 -0700

    first commit of what's my name

---
+++ 0x0B-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - prints the name of the calling file
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+int main (int argc, char *argv[])
+{
+	if (argc >= 1)
+		printf("%s\n", argv[0]);
+	return (0);
+}
