029326d50147c54359b89c1c298b4d4526571831
jswini <jswinikoff@gmail.com>
2021-07-29 12:53:45 -0700

    removed extra if

--- 0x11-variadic_functions/1-print_numbers.c
+++ 0x11-variadic_functions/1-print_numbers.c
@@ -19,16 +19,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		if (i != (n - 1))
 		{
-			if (i != (n - 1))
+			if (separator != NULL)
 			{
-				if (separator != NULL)
-				{
-					printf("%d%s", va_arg(ap, int), separator);
-				}
-				else
-				{
-					printf("%d", va_arg(ap, int));
-				}
+				printf("%d%s", va_arg(ap, int), separator);
+			}
+			else
+			{
+				printf("%d", va_arg(ap, int));
 			}
 		}
 		else
