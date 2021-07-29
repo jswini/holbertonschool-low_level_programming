174c3949b6317c505cabd6f6a9bbeafcd660eb45
jswini <jswinikoff@gmail.com>
2021-07-29 12:02:13 -0700

    added separator check

--- 0x11-variadic_functions/1-print_numbers.c
+++ 0x11-variadic_functions/1-print_numbers.c
@@ -2,6 +2,20 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
+ * _isdigit - checks input for if it is a digit
+ * @c: input
+ *
+ *Return: 1 if digit, 0 if not
+ */
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+		return (1);
+	else
+		return (0);
+}
+
+/**
  * print_numbers - outputs a list of numbers
  * @separator: text to separate numbers
  * @n: number of ints passed
@@ -14,6 +28,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(ap, n);
+
+	if (separator == NULL)
+		separator = "";
 
 	for (i = 0; i < n; i++)
 	{
