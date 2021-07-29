672ac338214ca653cd4a265ea9febaa330cb0d71
jswini <jswinikoff@gmail.com>
2021-07-29 12:33:23 -0700

    changed separator check to skip separator if null)

--- 0x11-variadic_functions/1-print_numbers.c
+++ 0x11-variadic_functions/1-print_numbers.c
@@ -15,14 +15,21 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(ap, n);
 
-	if (separator == NULL)
-		separator = "";
-
 	for (i = 0; i < n; i++)
 	{
 		if (i != (n - 1))
 		{
-			printf("%d%s", va_arg(ap, int), separator);
+			if (i != (n - 1))
+			{
+				if (separator != NULL)
+				{
+					printf("%d%s", va_arg(ap, int), separator);
+				}
+				else
+				{
+					printf("%d", va_arg(ap, int));
+				}
+			}
 		}
 		else
 		{
