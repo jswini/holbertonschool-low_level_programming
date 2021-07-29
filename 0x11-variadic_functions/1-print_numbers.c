55a942d3976ac39ca9759c27757b1a6d78fe61ad
jswini <jswinikoff@gmail.com>
2021-07-29 12:07:46 -0700

    added va_end

--- 0x11-variadic_functions/1-print_numbers.c
+++ 0x11-variadic_functions/1-print_numbers.c
@@ -2,20 +2,6 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
- * _isdigit - checks input for if it is a digit
- * @c: input
- *
- *Return: 1 if digit, 0 if not
- */
-int _isdigit(int c)
-{
-	if ((c >= 48) && (c <= 57))
-		return (1);
-	else
-		return (0);
-}
-
-/**
  * print_numbers - outputs a list of numbers
  * @separator: text to separate numbers
  * @n: number of ints passed
@@ -43,4 +29,5 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			printf("%d\n", va_arg(ap, int));
 		}
 	}
+	va_end(ap);
 }
