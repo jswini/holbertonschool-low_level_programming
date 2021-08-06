00084eed595ac9e86e2858054a782fcdbe401aeb
jswini <jswinikoff@gmail.com>
2021-08-06 10:39:23 -0700

    first commit of print list

---
+++ 0x13-singly_linked_lists/0-print_list.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_list - prints a linked list
+ * @h: head of the list
+ *
+ * Return: number of nodes in the list printed
+ */
+size_t print_list(const list_t *h)
+{
+	int count, i;
+	char *error = "[0] (nil)";
+
+	for (count = 0; h != NULL; count++)
+	{
+		if (h->str == NULL)
+		{
+			for (i = 0; error[i] != '\0'; i++)
+				_putchar(error[i]);
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('[');
+			_putchar(h->len + '0');
+			_putchar(']');
+			_putchar(' ');
+			for (i = 0; h->str[i] != '\0'; i++)
+			{
+				_putchar(h->str[i]);
+			}
+			_putchar('\n');
+		}
+		h = h->next;
+	}
+	return (count);
+}
