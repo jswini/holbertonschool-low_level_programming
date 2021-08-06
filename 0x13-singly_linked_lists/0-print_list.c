454c54576f92eefb55e8b52df91a95fed7a89e2f
jswini <jswinikoff@gmail.com>
2021-08-06 10:50:55 -0700

    added to account for empty string

--- 0x13-singly_linked_lists/0-print_list.c
+++ 0x13-singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 
 	for (count = 0; h != NULL; count++)
 	{
-		if (h->str == NULL)
+		if ((h->str == NULL) || (*h->str == '\0'))
 		{
 			for (i = 0; error[i] != '\0'; i++)
 				_putchar(error[i]);
