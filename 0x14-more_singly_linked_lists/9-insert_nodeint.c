8f3acdf21de3169d333c97f29beaa764f9758c8e
jswini <jswinikoff@gmail.com>
2021-08-11 13:16:52 -0700

    First commit of insert node int

---
+++ 0x14-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - adds a node to a linked list at a specific point
+ * @head: start of list
+ * @idx: index where node should be added
+ * @n: data to add to node
+ *
+ * Return: address of node if success, NULL if fail
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *current;
+	listint_t *new_node;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	current = *head;
+	while (current != NULL)
+	{
+		if (i == idx - 1)
+		{
+			new_node->n = n;
+			new_node->next = current->next;
+			current->next = new_node;
+			return (new_node);
+		}
+		i++;
+		current = current->next;
+	}
+	return (NULL);
+}
