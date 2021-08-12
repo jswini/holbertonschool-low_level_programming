c491de1dd593e62eefc6986c2640120be194d64e
jswini <jswinikoff@gmail.com>
2021-08-12 12:56:02 -0700

    removed helper function

--- 0x15-bit_manipulation/1-print_binary.c
+++ 0x15-bit_manipulation/1-print_binary.c
@@ -1,19 +1,5 @@
 #include "holberton.h"
 /**
- * numdigits - checks number of binary digits in a number
- * @n: number to check
- *
- * Return: number of binary digits
- */
-int numdigits(unsigned long int n)
-{
-	int count;
-
-	for (count = 0; n > 0; count++)
-		n /= 2;
-	return (count);
-}
-/**
  * print_binary - outputs the binary of a decimal input
  * @n: decimal to convert
  *
@@ -22,15 +8,8 @@ int numdigits(unsigned long int n)
 void print_binary(unsigned long int n)
 {
 	unsigned long int i, digit;
-	int size;
 
-	size = numdigits(n);
-	if (size == 0)
-	{
-		_putchar(0 + '0');
-		return;
-	}
-	for (i = 1 << (size - 1); i > 0; i >>= 1)
+	for (i = 1 << 10; i > 0; i >>= 1)
 	{
 		if (n & i)
 			digit = 1;
