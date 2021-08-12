1a05be1c7d846781535da53049f769c78c7cbb33
jswini <jswinikoff@gmail.com>
2021-08-12 13:17:31 -0700

    refactored to recursive to format properly

--- 0x15-bit_manipulation/1-print_binary.c
+++ 0x15-bit_manipulation/1-print_binary.c
@@ -7,14 +7,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int i, digit;
-
-	for (i = 1 << 10; i > 0; i >>= 1)
-	{
-		if (n & i)
-			digit = 1;
-		else
-			digit = 0;
-		_putchar(digit + '0');
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
