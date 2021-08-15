439d4f5bfa41385367bd2a85d94d7305e4b1b2c5
jswini <jswinikoff@gmail.com>
2021-08-15 15:35:20 -0700

    refactored to stop seg faults

--- 0x14-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x14-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,23 +15,34 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 	unsigned int i = 0;
 
-	if (*head == NULL)
+	if (!head || (*head == NULL && idx > 0))
 		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	current = *head;
-	while (current != NULL)
+	if (idx == 0)
 	{
-		if (i == idx)
+		new_node->n = n;
+		new_node->next = NULL;
+		*head = new_node;
+		return (new_node);
+	}
+	else
+	{
+		current = *head;
+		for (i = 1; current && i < idx; i++)
+			current = current->next;
+		if (i != idx)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		else
 		{
 			new_node->n = n;
 			new_node->next = current->next;
 			current->next = new_node;
-			return (new_node);
 		}
-		i++;
-		current = current->next;
 	}
-	return (NULL);
+	return (new_node);
 }
