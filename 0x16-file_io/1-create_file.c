7a55df6b6fdb9a997943f372cc95d1214d02e17c
jswini <jswinikoff@gmail.com>
2021-08-17 11:49:53 -0700

    changed null input check for text

--- 0x16-file_io/1-create_file.c
+++ 0x16-file_io/1-create_file.c
@@ -21,12 +21,14 @@ int create_file(const char *filename, char *text_content)
 	for (i = 0; text_content[i] != '\0'; i++)
 	{}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 	if (fd == -1)
 		return (-1);
-	if (text_content == NULL)
-		write(fd, text_content, 0);
-	else
-		write(fd, text_content, i);
+	write(fd, text_content, i);
 	close(fd);
 	return (1);
 }
