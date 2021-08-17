8112d34df2e51c5a8ae7a29cfad8e5b2309d3083
jswini <jswinikoff@gmail.com>
2021-08-17 11:31:37 -0700

    first commit of create file

---
+++ 0x16-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+/**
+ * create_file - creates a file, or truncates if it exists
+ * @filename: name of file to create
+ * @text_content: text to include in the file
+ *
+ * Return: 1 on success, -1 for failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int i;
+
+	if (filename == NULL)
+		return (-1);
+	for (i = 0; text_content[i] != '\0'; i++)
+	{}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		write(fd, text_content, 0);
+	else
+		write(fd, text_content, i);
+	close(fd);
+	return (1);
+}
