d3857e8b2d6694f89ff9e6885ae00eda223fb356
jswini <jswinikoff@gmail.com>
2021-09-09 13:28:51 -0500

    fixed for linter

--- 0x18-doubly_linked_lists/0-print_dlistint.c
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/*
+/**
 * print_dlistint - output the contents of a doubly linked list
-* h: head node of list to print
+* @h: head node of list to print
 *
 * Return: number of nodes to print
 */
@@ -12,7 +12,7 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count;
-	
+
 	if (h == NULL)
 		return (0);
 	for (count = 0; h != NULL; count++)
