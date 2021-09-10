2af7914b2bbe2f3ee0da211bc8c077fd3f79b062
jswini <jswinikoff@gmail.com>
2021-09-10 13:50:59 -0500

    first commit add node at begin

---
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_dnodeint - adds a node to the start of the list
+ * @head: first node of the list
+ * @n: valude to add to node
+ *
+ * Return: address of node if success, NULL if failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = (*head);
+	new_node->prev = NULL;
+	if (head != NULL)
+		(*head) = new_node;
+	(*head)->prev = new_node;
+	return (new_node);
+}
