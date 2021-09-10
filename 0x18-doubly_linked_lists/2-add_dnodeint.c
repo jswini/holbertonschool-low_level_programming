3cf38c3531afcf0889a910f2f06e5b291a276f78
jswini <jswinikoff@gmail.com>
2021-09-10 14:00:11 -0500

    freed if malloc fails

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -15,7 +15,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
+	{
+		free(new_node);
 		return (NULL);
+	}
 	new_node->n = n;
 	new_node->next = (*head);
 	new_node->prev = NULL;
