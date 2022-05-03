509e770cf205ecf13f3f2cd49ee4480d1394cfbb
jswini <jswinikoff@gmail.com>
2022-05-03 15:28:35 -0500

    fixed logic so each condition had a test

--- 0x00-makefiles/5-island_perimeter.py
+++ 0x00-makefiles/5-island_perimeter.py
@@ -4,21 +4,23 @@
 
 def island_perimeter(grid):
     '''This function accepts grid and returns the perimeter'''
-    row_length = len(grid)
-    col_len = len(grid[0])
+    lrow = len(grid)
+    lcol = len(grid[0])
     north = 0
     east = 0
     south = 0
     west = 0
-
-    for i in range(0, row_length):
-        for j in range(0, col_len):
-            if grid[i][j] == 1 and (j - 1 < 0 or grid[i][j - 1] == 0):
-                north += 1
-            if grid[i][j] == 1 and (i + 1 >= row_length or grid[i + 1][j]):
-                east += 1
-            if grid[i][j] == 1 and (j + 1 >= col_len or grid[i][j + 1] == 0):
-                south += 1
-            if grid[i][j] == 1 and (i - 1 < 0 or grid[i - 1][j] == 0):
-                west += 1
+    if grid is None:
+        return 0
+    else:
+        for i in range(0, lrow):
+            for j in range(0, lcol):
+                if grid[i][j] == 1 and (j - 1 < 0 or grid[i][j - 1] == 0):
+                    north += 1
+                if grid[i][j] == 1 and (i + 1 >= lrow or grid[i + 1][j] == 0):
+                    east += 1
+                if grid[i][j] == 1 and (j + 1 >= lcol or grid[i][j + 1] == 0):
+                    south += 1
+                if grid[i][j] == 1 and (i - 1 < 0 or grid[i - 1][j] == 0):
+                    west += 1
     return north + east + south + west
