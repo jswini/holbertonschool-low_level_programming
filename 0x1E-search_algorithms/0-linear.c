7c9bcea56256688bd6a0f85fd768943ad62d5af3
jswini <jswinikoff@gmail.com>
2022-06-13 14:41:43 -0500

    corrected return, and fixed for linter

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,22 +1,22 @@
 #include "search_algos.h"
 /**
- * @linear_search - finds target by checking each element
- * 
+ * linear_search - finds target by checking each element
+ *
  * @array: list of items to search
- * @size: size of list 
- * @value: target of search 
- * 
- * return: result of search
+ * @size: size of list
+ * @value: target of search
+ *
+ * Return: result of search
  */
 int linear_search(int *array, size_t size, int value)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%d] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return(value);
-    }
-    return(-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
